152a9b49103b59b31da35a46809ce3e1e89f7486
orphandeity <88175879+orphandeity@users.noreply.github.com>
2023-04-10 23:10:37 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+function App() {
+  return (
+    <main>
+      <button>Free Consultation</button>
+
+      <p className="heading-lg">Design solutions made easy</p>
+
+      <p>
+        With over ten years of experience in various design disciplines,
+        I&apos;m your one-stop shop for your design needs.
+      </p>
+
+      <span>Graphic Design</span>
+      <span>UI/UX</span>
+      <span>Apps</span>
+      <span>Photography</span>
+      <span>Illustrations</span>
+      <span>Motion Graphics</span>
+
+      <p>I&apos;m Amy, and I&apos;d love to work on your next project</p>
+
+      <p>
+        I love working with others to create beautiful design solutions.
+        I&apos;ve designed everything from brand illustrations to complete
+        mobile apps. I&apos;m also handy with a camera!
+      </p>
+
+      <button>Free Consultation</button>
+
+      <p>My Work</p>
+
+      <p>Book a call with me</p>
+
+      <p>
+        I&apos;d love to have a chat to see how I can help you. The best first
+        step is for us to discuss your project during a free consultation. Then
+        we can move forward from there.
+      </p>
+
+      <button>Free Consultation</button>
+
+      <button>Free Consultation</button>
+    </main>
+  );
+}
+
+export default App;
