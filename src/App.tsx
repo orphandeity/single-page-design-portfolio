d092780ef8e19a12ca2e0889aa5ffdab7deb1a4f
orphandeity <88175879+orphandeity@users.noreply.github.com>
2023-04-11 00:25:42 -0700

    mobile layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,68 @@
+import logo from "./assets/logo.svg";
+import profileImage from "./assets/image-amy.webp";
+
 function App() {
   return (
-    <main>
-      <button>Free Consultation</button>
+    <div className="container">
+      <header>
+        <img src={logo} alt="" className="logo" />
+        <button>Free Consultation</button>
+      </header>
 
-      <p className="heading-lg">Design solutions made easy</p>
+      <div className="section hero">
+        <h1 className="heading-lg">Design solutions made easy</h1>
+        <p>
+          With over ten years of experience in various design disciplines,
+          I&apos;m your one-stop shop for your design needs.
+        </p>
+      </div>
 
-      <p>
-        With over ten years of experience in various design disciplines,
-        I&apos;m your one-stop shop for your design needs.
-      </p>
+      <div className="grid-container">
+        <div className="grid-item">Graphic Design</div>
+        <div className="grid-item">UI/UX</div>
+        <div className="grid-item">Apps</div>
+        <div className="grid-item">Photography</div>
+        <div className="grid-item">Illustrations</div>
+        <div className="grid-item">Motion Graphics</div>
+      </div>
 
-      <span>Graphic Design</span>
-      <span>UI/UX</span>
-      <span>Apps</span>
-      <span>Photography</span>
-      <span>Illustrations</span>
-      <span>Motion Graphics</span>
+      <div className="section bio">
+        <img src={profileImage} alt="" className="profile-image" />
 
-      <p>I&apos;m Amy, and I&apos;d love to work on your next project</p>
+        <div className="section">
+          <h2 className="heading-md">
+            I&apos;m Amy, and I&apos;d love to work on your next project
+          </h2>
 
-      <p>
-        I love working with others to create beautiful design solutions.
-        I&apos;ve designed everything from brand illustrations to complete
-        mobile apps. I&apos;m also handy with a camera!
-      </p>
+          <p>
+            I love working with others to create beautiful design solutions.
+            I&apos;ve designed everything from brand illustrations to complete
+            mobile apps. I&apos;m also handy with a camera!
+          </p>
 
-      <button>Free Consultation</button>
+          <button>Free Consultation</button>
+        </div>
+      </div>
 
-      <p>My Work</p>
+      <h3 className="heading-sm">My Work</h3>
 
-      <p>Book a call with me</p>
+      <div className="section cta">
+        <div>
+          <h2 className="heading-md">Book a call with me</h2>
+          <p>
+            I&apos;d love to have a chat to see how I can help you. The best
+            first step is for us to discuss your project during a free
+            consultation. Then we can move forward from there.
+          </p>
+        </div>
+        <button>Free Consultation</button>
+      </div>
 
-      <p>
-        I&apos;d love to have a chat to see how I can help you. The best first
-        step is for us to discuss your project during a free consultation. Then
-        we can move forward from there.
-      </p>
-
-      <button>Free Consultation</button>
-
-      <button>Free Consultation</button>
-    </main>
+      <footer>
+        <img src={logo} alt="" className="logo" />
+        <button>Free Consultation</button>
+      </footer>
+    </div>
   );
 }
 
